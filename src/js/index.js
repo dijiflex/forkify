f6bdf09075873b3d74053e42f462174ec3cef0eb
dijiflex <dijiflex@gmail.com>
2019-11-15 18:07:49 +0300

    Installed babel core

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 // Global app controller
 import  num from './test';
 
-console.log(`i imported ${num} from another module`);
+console.log(`i imported ${num} from another module and variable x is `);
 
